41d499a5c517e093415c3a645fb1d9242767d01b
yangweicong <yangweicong@nicomama.com>
2019-07-15 11:28:03 +0800

    week

--- README.md
+++ README.md
@@ -2,14 +2,29 @@
 
 ## Configuration
 
-`"prefix": "######"`
+- `"prefix": "######"`
 The tag before moment.
 
-`"format": "YYYY/MM/DD ddd HH:mm"`
+- `"format": "YYYY/MM/DD ddd HH:mm"`
 Moment format.
 
-`"list": ["今日计划", "今日记录", "明日计划"]`
+- `"list": ["学习", "产出", "想法", "计划"]`
 Daily record.
+
+- `"weekPrefix": "#####"`
+The tag before week.
+
+- `"mode": "today"`
+Shortcuts action (today, week, nextWeek, weekWithDay, nextWeekWithDay).
+
+- `"firstDay": 0`
+First day of the week (0: Sunday, 1: Monday, etc).
+
+- `"rangeSeparator": " - "`
+Range separator between week start and week end.
+
+- `"weekList": ["本周计划", "本周总结"]`
+Week record.
 
 
 ## Shortcut
@@ -19,16 +34,123 @@ Default shortcut is `ctrl + 5`  / `cmd + 5`
 
 ## Example
 
-```markdown
+- DayComment: Today
+    ```md
+###### 2019/07/15 Mon 
+- 学习
+- 产出
+- 想法
+- 计划
+    ```
+- DayComment: Week
+    ```md
+##### 2019/07/14 - 2019/07/20 
+- 本周计划
+- 本周总结
+    ```
+- DayComment: Next Week
+    ```md
+##### 2019/07/21 - 2019/07/27 
+- 本周计划
+- 本周总结
+    ```
+- DayComment: Week WithDay
+    ```md
+##### 2019/07/14 - 2019/07/20 
+- 本周计划
+- 本周总结
 
-###### 2019/01/09 Wed 22:20
-- 今日计划
+###### 2019/07/14 Sun
+- 学习
+- 产出
+- 想法
+- 计划
 
-- 今日记录
+###### 2019/07/15 Mon
+- 学习
+- 产出
+- 想法
+- 计划
 
-- 明日计划
+###### 2019/07/16 Tue
+- 学习
+- 产出
+- 想法
+- 计划
 
-```
+###### 2019/07/17 Wed
+- 学习
+- 产出
+- 想法
+- 计划
+
+###### 2019/07/18 Thu
+- 学习
+- 产出
+- 想法
+- 计划
+
+###### 2019/07/19 Fri
+- 学习
+- 产出
+- 想法
+- 计划
+
+###### 2019/07/20 Sat
+- 学习
+- 产出
+- 想法
+- 计划
+    ```
+- DayComment: Next Week WithDay
+    ```md
+##### 2019/07/21 - 2019/07/27 
+- 本周计划
+- 本周总结
+
+###### 2019/07/21 Sun
+- 学习
+- 产出
+- 想法
+- 计划
+
+###### 2019/07/22 Mon
+- 学习
+- 产出
+- 想法
+- 计划
+
+###### 2019/07/23 Tue
+- 学习
+- 产出
+- 想法
+- 计划
+
+###### 2019/07/24 Wed
+- 学习
+- 产出
+- 想法
+- 计划
+
+###### 2019/07/25 Thu
+- 学习
+- 产出
+- 想法
+- 计划
+
+###### 2019/07/26 Fri
+- 学习
+- 产出
+- 想法
+- 计划
+
+###### 2019/07/27 Sat
+- 学习
+- 产出
+- 想法
+- 计划
+    ```
+
 
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,72 @@
 import * as vscode from 'vscode'
 import * as moment from 'moment'
 
+enum Mode {
+	today = 'today',
+	week = 'week',
+	nextWeek = 'nextWeek',
+	weekWithDay = 'weekWithDay',
+	nextWeekWithDay = 'nextWeekWithDay',
+}
+
+type Action = 'action'
+
 interface Contributions {
 	prefix: string
+	weekPrefix: string
+	firstDay: number
+	rangeSeparator: string
+	mode: Mode
 	format: string
 	list: Array<string>
+	weekList: Array<string>
+}
+
+type CommandFun = (contributions: Contributions) => string
+
+const actionMap: Map<Mode | Action, CommandFun> = new Map()
+	.set(Mode.today, getDayComment)
+	.set(Mode.week, getWeekComment(0, false))
+	.set(Mode.nextWeek, getWeekComment(1, false))
+	.set(Mode.weekWithDay, getWeekComment(0, true))
+	.set(Mode.nextWeekWithDay, getWeekComment(1, true))
+
+const commands: Map<Mode | Action, CommandFun> = new Map(actionMap)
+	.set('action', action)
+
+function action(contributions: Contributions): string {
+	const mode: Mode = contributions.mode
+	const act = actionMap.get(mode)
+	return act ? act(contributions) : ''
 }
 
 function getDayComment(contributions: Contributions): string {
-	const prefix: string = contributions.prefix
-	const format: string = contributions.format
-	const list: Array<string> = contributions.list
+	const { prefix, format, list } = contributions
 
-	return `\n${prefix} ${formatDay(format)} \n- ${list.join('\n\n- ')}\n`
+	return `\n${prefix} ${formatDay(format)} \n- ${list.join('\n- ')}\n`
 }
 
-function getWeekComment(): string {
-	const dayStart: string = moment().set('weekday', 1).format('YYYY/MM/DD')
-	const dayEnd: string = moment().set('weekday', 7).format('YYYY/MM/DD')
+function getWeekComment(weekNum: number, showAllDays: boolean): CommandFun {
+	return (contributions: Contributions) => {
+		const {
+			prefix,
+			weekPrefix,
+			rangeSeparator,
+			list,
+			weekList,
+			firstDay,
+		} = contributions
+		const firstD: number = firstDay % 7
 
-	return `\n##### ${dayStart} - ${dayEnd}`
+		const weekStartDay = moment().add('week', weekNum).set('weekday', firstD)
+		const dayStart: string = weekStartDay.format('YYYY/MM/DD')
+		const dayEnd: string = moment(weekStartDay).add('d', 6).format('YYYY/MM/DD')
+	
+		return `\n${weekPrefix} ${dayStart}${rangeSeparator}${dayEnd} \n- ${weekList.join('\n- ')}\n`
+			+ (showAllDays ? Array.from(Array(7)).map((_, index) => {
+				return `\n${prefix} ${moment(weekStartDay).add('d', index).format('YYYY/MM/DD ddd')}\n- ${list.join('\n- ')}`
+			}).join('\n') + '\n' : '')
+	}
 }
 
 function formatDay(format: string): string {
@@ -29,42 +76,32 @@ function formatDay(format: string): string {
 }
 
 export function activate(context: vscode.ExtensionContext) {
+	for (let key of commands.keys()) {
+		const fun = commands.get(key)
 
-	let disposableDay = vscode.commands.registerCommand('extension.dayComment', () => {
-		const editor = vscode.window.activeTextEditor
-		if (editor) {
-			const selections = editor.selections
-			const contributions: Contributions = vscode.workspace.getConfiguration('day-comment') as any
-
-			const text = getDayComment(contributions)
-
-			editor.edit((editBuilder) => {
-				selections.forEach((selection) => {
-					editBuilder.replace(selection, '')
-					editBuilder.insert(selection.active, text)
-				})
-			})
+		if (!fun) {
+			continue
 		}
-	})
 
-	let disposableWeek = vscode.commands.registerCommand('extension.weekComment', () => {
-		const editor = vscode.window.activeTextEditor
-		if (editor) {
-			const selections = editor.selections
+		const command = vscode.commands.registerCommand(`DayComment.${key}`, () => {
+			const editor = vscode.window.activeTextEditor
+			if (editor) {
+				const contributions: Contributions = vscode.workspace.getConfiguration('day-comment') as any
+				const selections = editor.selections
 
-			const text = getWeekComment()
+				const text = fun(contributions)
 
-			editor.edit((editBuilder) => {
-				selections.forEach((selection) => {
-					editBuilder.replace(selection, '')
-					editBuilder.insert(selection.active, text)
+				editor.edit((editBuilder) => {
+					selections.forEach((selection) => {
+						editBuilder.replace(selection, '')
+						editBuilder.insert(selection.active, text)
+					})
 				})
-			})
-		}
-	})
+			}
+		})
 
-	context.subscriptions.push(disposableDay)
-	context.subscriptions.push(disposableWeek)
+		context.subscriptions.push(command)
+	}
 }
 
 export function deactivate() {}
